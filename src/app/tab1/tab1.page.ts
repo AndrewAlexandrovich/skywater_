85d2add3304c688a2245983002d9d414af3ed0a8
AndrewAlexandrovich <aaleksandrovich453@gmail.com>
2024-02-21 13:20:05 +0200

    Home svg, chat and qr

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,9 +11,15 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  public progress = 0.5;
+  public progress = '0, 700';
+  public progress_l = '0, 700';
+  public svg_class = 'circle-chart';
   public sliderItems = [];
   public categoryProducts = [];
+  public bonuses_boutles:any = 0;
+  public templ_b_bottle:any = 0;
+  public templ_b_liter:any = 0;
+  public bonuses_liters:any = 0;
 
   openProduct(product_id:any){
 	this.router.navigate(['product',{product_id:product_id}])
@@ -56,12 +62,52 @@ export class Tab1Page {
 
 	  let homeData = JSON.parse(JSON.stringify(response));
 	  this.sliderItems = homeData.slider;
-	  console.log(homeData);
 	  this.categoryProducts = homeData.categories_products;
 
+    if(homeData.bonuses_boutles){
+      let bounese_boutles = homeData.bonuses_boutles;
+          this.templ_b_bottle = homeData.bonuses_boutles;
+      let percent = ((bounese_boutles * 10) / 100) * 700;
+      this.progress = percent+', 700';
+      this.intervalSetBootles;
+    }else{
+      this.progress = '0, 700';
+      this.bonuses_boutles = 0;
+    }
+
+    if(homeData.bonuses_liters){
+      let bounese_l = homeData.bonuses_liters;
+          this.templ_b_liter = homeData.bonuses_liters;
+      let percent = ((bounese_l * 19) / 100) * 700;
+      if(percent > 500){
+        percent = percent / 1.5;
+      }
+      this.progress_l = percent+', 700';
+      this.intervalSetLiters;
+
+    }else{
+      this.progress_l = '0, 700';
+      this.bonuses_liters = 0;
+    }
+
 	});
+}
+
+  public intervalSetBootles = setInterval(() => {
+    this.bonuses_boutles++;
+    if(this.bonuses_boutles == this.templ_b_bottle){
+      clearInterval(this.intervalSetBootles)
+    }
+  }, 1000);
+  public intervalSetLiters = setInterval(() => {
+    this.bonuses_liters++;
+    if(this.bonuses_liters == this.templ_b_liter){
+      clearInterval(this.intervalSetLiters)
+    }
+  }, 700);
 
 
+ngOnInit(){
 
 }
 
