6e92d07058be325900561ca2eddd04a3386c61b9
ProAppStudia <146301256+ProAppStudia@users.noreply.github.com>
2024-03-22 11:27:28 +0200

    Revert "Ios"
    This reverts commit 3310174795df801f46c493f2d9c978b31b90aa48.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Sky Water',
-  webDir: 'www/browser',
+  webDir: 'www',
   server: {
     androidScheme: 'https'
   }
