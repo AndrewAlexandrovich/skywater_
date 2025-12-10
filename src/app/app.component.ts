8a5e66a1f9dce80aab5de90ce4dedb217105c30a
AndrewAlexandrovich <aaleksandrovich453@gmail.com>
2025-12-10 17:57:12 +0200

    safe are

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,38 @@ export class AppComponent {
 
     // Опціонально — зробити темний або світлий текст
     await StatusBar.setStyle({ style: Style.Dark });
+	
+	this.applySafeArea();
   }
+  
+  
+  applySafeArea() {
+  const computedTop = parseInt(
+    getComputedStyle(document.documentElement)
+      .getPropertyValue("env(safe-area-inset-top)"),
+    10
+  );
+
+  const computedBottom = parseInt(
+    getComputedStyle(document.documentElement)
+      .getPropertyValue("env(safe-area-inset-bottom)"),
+    10
+  );
+
+  // ---- TOP FIX: якщо більше 40px → вважаємо це баг
+  const safeTop =
+    Number.isFinite(computedTop) && computedTop > 0 && computedTop < 40
+      ? computedTop
+      : 0;
+
+  // ---- BOTTOM FIX: якщо більше 30px → вважаємо це баг
+  const safeBottom =
+    Number.isFinite(computedBottom) && computedBottom > 0 && computedBottom < 30
+      ? computedBottom
+      : 0;
+
+  document.documentElement.style.setProperty("--real-safe-top", safeTop + "px");
+  document.documentElement.style.setProperty("--real-safe-bottom", safeBottom + "px");
+}
+  
 }
